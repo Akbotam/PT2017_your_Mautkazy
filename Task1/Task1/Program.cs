c64a3a174c925d257cea11016d14499c56caf918
Akbota <bmautkazy@gmail.com>
2017-03-09 11:18:02 +0500

    comment

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            s = Console.ReadLine();
+            int N = int.Parse(s);
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
