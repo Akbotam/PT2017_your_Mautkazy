839da1f720e2afa1e1d528f690249b318ffc70a5
Akbota <bmautkazy@gmail.com>
2017-03-09 12:26:25 +0500

    comment

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Андрей\ PT2017_your_Mautkazy\input.txt");
+            StreamWriter sw = new StreamWriter(@"C:\Users\Андрей\ PT2017_your_Mautkazy\output.txt");
+            string s = sr.ReadLine();
+            string[] ss = s.Split();
+            int n = ss.Length;
+            int[] arr = new int[n];
+           int A  = int.Parse(ss[0]);
+            int B = int.Parse(ss[1]);
+                 if (A < B)
+                 {
+                     for (int i = 1; i < A; i++)
+                     {
+                         if (A % i == 0 && B % i == 0)
+                         {
+                             sw.Write(i + " ");
+                         } 
+                     }
+                 }
+                 else
+                 {
+                     for (int i = 1; i < B; i++)
+                     {
+                         if (A % i == 0 && B% i == 0)
+                         {
+                             sw.Write(i + " ");
+                         }
+                     }
+
+                 }
+              
+
+
+               /* for (int i = 0; i < n; i++)
+                 {
+                     arr [i] = int.Parse(ss[i]);
+               
+                 }
+                 for (int i = 0; i < n; i++)
+                 {
+                     if (arr[i] < arr[i + 1])
+                     {
+                         for (int j = 1; j < arr[i]; j++)
+                         {
+                             if (arr[i] % j == 0 && arr[i + 1] % j == 0)
+                             {
+                                 sw.WriteLine(j + " ");
+                             }
+                         }
+                     }
+                     else
+                     {
+                         for (int j = 1; j < arr[i+1]; j++)
+                         {
+                             if (arr[i] % j == 0 && arr[i + 1] % j == 0)
+                             {
+                                 sw.WriteLine(j + " ");
+                             }
+                         }
+
+                     }
+                 }  */
+                sr.Close();
+            sw.Close();
+            Console.ReadKey();
+           
+
+        }
+    }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mid
+{
+    class Program
+    {
+        /* static void comp(string path)
+         {   
+             Console.Clear();
+         FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+         StreamReader sr = new StreamReader(fs);
+         StreamWriter sw = new StreamWriter(fs);
+         string s = sr.ReadToEnd();
+         if (s == "abcd")
+         Console.WriteLine(s);
+         sr.Close();
+       
+         fs.Close();
+         Console.ReadKey();
+
+         } */
+
+        static void Main(string[] args)
+        {
+           /* string s;
+            s = Console.ReadLine();
+            string[] ss = s.Split();
+            int n = ss.Length;
+
+              DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Андрей\ PT2017_your_Mautkazy\Yes");
+            FileSystemInfo[] infos = dir.GetFileSystemInfos();
+            for (int i = 0; i < dir.GetFileSystemInfos().Length; i++)
+            {
+                if (infos[i].GetType() == typeof(FileInfo))
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < infos[j].Length; j++)
+                        {
+                            if (ss[i] == infos[j].Name)
+                            {
+                                Console.WriteLine(infos[i].FullName);
+                            }
+                        }
+                    }
+                    }
+                } */
+
+            StreamReader sr = new StreamReader(Console.ReadLine());
+            string s = sr.ReadLine();
+            DirectoryInfo dir = new DirectoryInfo(s);
+            FileSystemInfo[] infos = dir.GetFileSystemInfos();
+            for (int i = 0; i < dir.GetFileSystemInfos().Length; i++)
+            {
+                if (infos[i].GetType() == typeof(FileInfo))
+                {
+                    FileStream fs = new FileStream(infos[i].FullName, FileMode.Open, FileAccess.ReadWrite);
+                    StreamReader sr = new StreamReader(fs);
+                    string s = sr.ReadToEnd();
+                    if (s == "test")
+                    {
+
+                        Console.WriteLine(infos[i].FullName);
+                    }
+                }
+               /* else
+                {
+                    FileStream fs = new FileStream(infos[i].FullName, FileMode.Open, FileAccess.ReadWrite);
+                    StreamReader sr = new StreamReader(fs);
+                    string s = sr.ReadToEnd();
+                    if (s == "test")
+                    {
+
+                        Console.WriteLine(infos[i].FullName);
+                    }
+
+                } 
+                */
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
